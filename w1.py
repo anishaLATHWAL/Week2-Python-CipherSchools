10ec91a84ca5b799d7e834fe626f2273ce51bdcd
Anisha <anishalathwalal1983@gmail.com>
2022-12-12 21:03:29 +0530

    To print the sum of first 10 natural numbers..

---
+++ w1.py
@@ -0,0 +1,14 @@
+# Sum from 1 to 10...
+# 1+2+3+4+5+6+7+8+9+10
+
+sum=0
+for i in range(1,11):
+    sum=sum+i
+print(sum)
+
+# if the in put is taken from the user..
+n= int(input('Enter the number here: '))
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+print(sum)
